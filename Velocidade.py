7151b6c7d9a477f33fa3006530e2429b571b3b7d
FranciscoFerreira2012 <shark11714@sharkcoders.pt>
2025-10-28 20:50:24 +0100

    Update Velocidade.py

--- Velocidade.py
+++ Velocidade.py
@@ -1,10 +1,10 @@
 velocidade = int(input("Qual é a sua velocidade?"))
 if velocidade>120:
     velocidadeExcesso = (velocidade-120)*4
-    print("Estás muito acima do limite permitido. A multa foi gerada no valor de {} euros".format(velocidadeExcesso))
+    mensagem = "Estás muito acima do limite permitido. A multa foi gerada no valor de {} euros".format(velocidadeExcesso)
 elif velocidade>80:
     velocidadeExcesso = (velocidade-80)*2
-    print("Estás acima do limite permitido. A multa foi gerada no valor de {} euros".format(velocidadeExcesso))
+    mensagem = "Estás acima do limite permitido. A multa foi gerada no valor de {} euros".format(velocidadeExcesso)
 else:
-    print("Está detro do limite. Boa viagem!")
-    
+    mensagem = "Está detro do limite. Boa viagem!"
+print(mensagem)    
