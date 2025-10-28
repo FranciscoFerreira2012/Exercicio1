12d70d64de87a5f2d360b895d581ae481d29f23b
FranciscoFerreira2012 <shark11714@sharkcoders.pt>
2025-10-28 20:54:30 +0100

    Update Velocidade.py

--- Velocidade.py
+++ Velocidade.py
@@ -6,5 +6,6 @@ elif velocidade>80:
     velocidadeExcesso = (velocidade-80)*2
     mensagem = "Estás acima do limite permitido. A multa foi gerada no valor de {} euros".format(velocidadeExcesso)
 else:
-    mensagem = "Está detro do limite. Boa viagem!"
+    mensagem = "Está dentro do limite. Boa viagem!"
 print(mensagem)    
+
