887b267fa02d238b35eeeb55424e5047d959dad2
FranciscoFerreira2012 <shark11714@sharkcoders.pt>
2025-10-28 19:49:31 +0100

    Update Ordenados e reajustes.py

--- Ordenados e reajustes.py
+++ Ordenados e reajustes.py
@@ -1,11 +1,11 @@
 ordenado = float(input("Qual é o seu ordenado atual?"))
-x = (ordenado*reajuste/100)
 if ordenado < 500:
    reajuste = 15
-   print("O seu ordenado reajustado é de", x)
 elif ordenado <= 1000:
    reajuste = 10
-   print("O seu ordenado reajustado é de", x)
 else:
    reajuste = 5
-   print("O seu ordenado reajustado é de", x)
+ordenadoAumento = (ordenado*reajuste)/100
+ordenadoFinal = ordenado + ordenadoAumento
+
+print(f"O seu ordenado reajustado é de{ordenadoFinal}")
